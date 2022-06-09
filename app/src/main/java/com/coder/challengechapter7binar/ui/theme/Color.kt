1cc687f8a16132d682a61aab588ea3060b0fdc7e
wahyuananta <wahyuanantajr@gmail.com>
2022-06-09 16:14:45 +0700

    Set up compose

---
+++ app/src/main/java/com/coder/challengechapter7binar/ui/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.coder.challengechapter7binar.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val colorPrimary = Color(0xFF032541)
+val colorPrimaryDark = Color(0xFF031D33)
+val colorAccent = Color(0xFF03DAC5)
